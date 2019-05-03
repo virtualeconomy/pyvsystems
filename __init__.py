263dec7edfbd4a7a1b13e222a6649efd4a8b9ed0
hiutin0 <xiaotian@v.systems>
2019-05-03 10:20:30 +0800

    revise contract test

--- __init__.py
+++ __init__.py
@@ -63,6 +63,6 @@ def set_throw_on_error(throw=True):
     global THROW_EXCEPTION_ON_ERROR
     THROW_EXCEPTION_ON_ERROR = throw
 
-
+from .account import *
 from .contract import *
 from .data_entry import *
